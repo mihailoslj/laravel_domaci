41e5e760e237a156b2eb64a1b7a89416d1a57dfb
mihailoslj <samozagit@gmail.com>
2022-07-14 23:56:08 +0200

    Kreirana tabela 'product' i prva migracija
    Napisani POST i GET zahtevi za unos u tabelu, odnosno vracanje cele tabele products.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/products', function(){
+    return Product::all(); //vraca sve proizvode
+});
+
+Route::post('/products', function(){ //unos u tabelu post pozivom
+    return Product::create([
+        'name' => 'Product one',
+        'slug' => 'product-one',
+        'description' => 'This is product one',
+        'price' => '9.99'
+    ]);
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
